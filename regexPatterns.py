64fc969a0edf5f34d43698bd725416fdf9bc6b88
Eniitan <enitanolawale0@gmail.com>
2024-11-12 11:01:58 +0100

    created the necessary patterns

---
+++ regexPatterns.py
@@ -0,0 +1,16 @@
+email_pattern: str = r"""(
+    [a-zA-Z-0-9._%+!~-]+ # username
+    @
+    [a-zA-Z-0-9.-]+ # domain name
+    (\.[a-zA-Z]{2,4}) # dot something
+)"""
+phone_no_pattern: str = r"\+\d{13}|d{11}"
+
+american_no_pattern: str = r"""(
+    (\(?\d{3}\)?) # area code, required
+    (\s|-|\.)?    # separator
+    (\d{3})       # first 3 digits
+    (\s|-|\.)     # separator
+    (\d{4})       # last 4 digits
+    (\s*(ext|x|ext\.)\s*(\d{2,5}))? # optional extension
+)"""
